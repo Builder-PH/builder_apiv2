5a3629a007e7833d3930cc613dff63fe32a91a81
Nirel Ibarra <62606372+knee-rel@users.noreply.github.com>
2021-11-09 20:57:59 +0800

    resolving cors error

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ const servicesData = require("./services.json");
 
 let port = process.env.PORT || 5000;
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+
+  next();
+});
+
 app.get("/", (req, res) => {
   res.send("Working");
 });
